214c4655e6dc01859e05553d19066d2a9752ef12
Dave Johnson <DJohnson@keoghs.co.uk>
2020-01-02 16:06:41 +0000

    First commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace CodeFirstSQLGenerator
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.ofdFile = new System.Windows.Forms.OpenFileDialog();
+            this.rtbSQL = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnBrowse);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1304, 43);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Controls";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(12, 14);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // rtbSQL
+            // 
+            this.rtbSQL.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbSQL.Location = new System.Drawing.Point(0, 43);
+            this.rtbSQL.Name = "rtbSQL";
+            this.rtbSQL.Size = new System.Drawing.Size(1304, 482);
+            this.rtbSQL.TabIndex = 3;
+            this.rtbSQL.Text = "";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1304, 525);
+            this.Controls.Add(this.rtbSQL);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmMain";
+            this.Text = "Code First SQL Generator";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.OpenFileDialog ofdFile;
+        private System.Windows.Forms.RichTextBox rtbSQL;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CodeFirstSQLGenerator
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, System.EventArgs e)
+        {
+            if (ofdFile.ShowDialog() == DialogResult.OK)
+            {
+                string line;
+                var sbLit = new System.Text.StringBuilder();
+                var file = new System.IO.StreamReader(ofdFile.FileName);
+
+                sbLit.AppendLine("var sqlBuilder = new StringBuilder();");
+                sbLit.AppendLine();
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (!string.IsNullOrEmpty(line))
+                    {
+                        sbLit.AppendLine("sqlBuilder.AppendLine(\"" + line + "\");");
+                    }
+                    else
+                    {
+                        sbLit.AppendLine("sqlBuilder.AppendLine();");
+                    }
+                }
+
+                file.Close();
+
+                sbLit.AppendLine();
+                sbLit.AppendLine("migrationBuilder.Sql(sqlBuilder.ToString());");
+
+                rtbSQL.Text = sbLit.ToString();
+
+            }
+        }
+    }
+}
